8b4f664c106c8a2595b6cf85b5a134f975319d7f
Jenny <jenny.thorne3006@gmail.com>
2018-11-17 20:53:51 +0000

    Better callback naming

--- radio.py
+++ radio.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(4, GPIO.RISING)
 
-def toggle_callback():
+def toggle_callback(channel):
     os.system("mpc toggle")
 
 GPIO.add_event_callback(4, toggle_callback)
