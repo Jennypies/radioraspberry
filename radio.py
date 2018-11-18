fdf08fe59eb682631c62f9333cdc85fbdb956174
Jenny <jenny.thorne3006@gmail.com>
2018-11-18 13:19:14 +0000

    blaaa

--- radio.py
+++ radio.py
@@ -5,11 +5,15 @@ from signal import pause
 
 toggle_switch = Button(4, pull_up=False)
 
-def toggle():
-    os.system("mpc toggle")
+next_switch = Button(17, pull_up=False, hold_time=0.5, hold_repeat=True,)
+
+previous_switch = Button(27, pull_up=False, hold_time=0.5, hold_repeat=True,)
 
 
+toggle_switch.when_pressed = os.system("mpc toggle")
 
-toggle_switch.when_pressed = toggle
+next_switch.hold_repeat = os.system("mpc next")
+
+previous_switch.hold_repeat = os.system("mpc prev")
 
 pause()
