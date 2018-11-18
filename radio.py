40ba7132da6c989dc144ef21bb9dbae1a66fa915
Jenny <jenny.thorne3006@gmail.com>
2018-11-18 11:04:47 +0000

    blaaa

--- radio.py
+++ radio.py
@@ -3,11 +3,13 @@ from gpiozero import Button
 from signal import pause
 
 
-toggle_switch = Button(4, pull_up=False, bounce_time=100, hold_time=1, hold_repeat=False, pin_factory=None)
+toggle_switch = Button(4, pull_up=False, bounce_time=0.2, hold_time=1, hold_repeat=False, pin_factory=None)
 
 def toggle():
     os.system("mpc toggle")
 
-    toggle_switch.when_pressed = toggle
+
+
+toggle_switch.when_pressed = toggle
 
 pause()
