8121a49b7fb982e41bbeeac86cb55f8ce773c7d9
Jenny <jenny.thorne3006@gmail.com>
2018-11-18 10:31:45 +0000

    blaaa

--- radio.py
+++ radio.py
@@ -4,7 +4,7 @@ from signal import pause
 
 toggle_switch = Button(4, pull_up=False, bounce_time=200, hold_time=2, hold_repeat=True, pin_factory=None)
 
-def toggle()):
+def toggle():
     os.system("mpc toggle")
 
 toggle_switch.when_pressed = toggle
