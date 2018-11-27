46bc71f43bf568af3895834e4db604d9f4252034
Jenny <jenny.thorne3006@gmail.com>
2018-11-27 20:36:30 +0000

    pushy

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 import subprocess
 
-subprocess.check_output(["echo", "Hello World!"])
-print(subprocess.check_output("mpc idle"))
-subprocess.check_output(["echo", "Hello World!"])
+print(subprocess.check_output(["mpc", "idle"])
