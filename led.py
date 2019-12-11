6b919b0e8d826222e7cabe0f9406ab825acb36ef
Jenny <jenny.thorne3006@gmail.com>
2019-12-11 22:04:19 +0000

    pushy

--- led.py
+++ led.py
@@ -10,55 +10,25 @@ next_led = LED(13)
 prev_led = LED(22)
 
 
-def startup_led():
-    for x in range(2):
-        toggle_led.on()
-        next_led.on()
-        prev_led.on()
-        sleep(1)
-        toggle_led.off()
-        next_led.off()
-        prev_led.off()
-        sleep(1)
-    toggle_led.on()
-    next_led.on()
-    prev_led.on()
-
-
-def pause_led():
-        toggle_led.on()
-        next_led.on()
-        prev_led.on()
-        sleep(1)
-        toggle_led.off()
-        next_led.off()
-        prev_led.off()
-        sleep(1)
-
-
-def led_fade_off():
-    toggle_led.off()
-    prev_led.off()
-    next_led.off()
-
-
 def main():
     try:
-        # initialise LED start up sequence
-        startup_led()
         # While loop for main program
         while True:
+            toggle_led.on()
+            next_led.on()
+            prev_led.on()
+            sleep(1)
             # check mpc status
             status = subprocess.check_output(["mpc", "status"])
             if b"paused" in status:
-                pause_led()
-            else:
-                toggle_led.on()
-                next_led.on()
-                prev_led.on()
-            subprocess.check_call(["mpc", "idle"])
+                toggle_led.off()
+                prev_led.off()
+                next_led.off()
+                sleep(1)
     finally:
-        led_fade_off()
+        toggle_led.off()
+        prev_led.off()
+        next_led.off()
 
 
 if __name__ == "__main__":
